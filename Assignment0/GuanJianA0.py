e1cd1467201119f46e542e6a62bffe65c3f7fa3f
Jian Guan <guanwwjiam@gmail.com>
2023-09-07 23:01:29 -0400

    add GuanJian's Assignment0

---
+++ Assignment0/GuanJianA0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 18:27:46 2023
+
+@author: guanw
+"""
+
+print("Welcome to Laurentian University!")
+print("Bienvenue à l'Université Laurentienne!")
