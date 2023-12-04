08ccc2832cf40b2f81571842143040ea0d9700bc
Jian Guan <guanwwjiam@gmail.com>
2023-12-04 07:37:45 -0500

    finish A4

--- Assignment4/CPSC_5616EL_A4_G8.py
+++ Assignment4/CPSC_5616EL_A4_G8.py
@@ -12,8 +12,6 @@ from imblearn.over_sampling import RandomOverSampler
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
 
 #%%
 # All the data should put into the data folder
@@ -88,6 +86,20 @@ for i in range(len(df_list)):
     
 # %%
 # Window by dividing the data of each exercise into eaqual parts to apply data augmentation we use several different window sizes
+
+# If the length of the array is less than the target length, pad it with zeros and make the original array at the beginning
+def pad(original_series, target_length):
+    new_series = original_series.copy()
+    for i in range(original_series.size):
+        line = original_series[i]
+        original_length = len(line)
+        if original_length > target_length:
+            new_line = line[:target_length]
+        else:
+            new_line = np.pad(line, ((0, target_length - original_length), (0, 0)), mode="constant")
+        new_series[i] = new_line
+    return new_series
+
 windows_count_list = [8, 9, 10, 11, 12]
 df_concat_list = []
 for windows_count in windows_count_list:
@@ -101,13 +113,20 @@ for windows_count in windows_count_list:
     print(seconds_elapsed_min, windows_size)
 
     # process window and calculate min, max, mean, std value of each window
+    count_after_padding = 200
     df_list_temp = []
-    for df in df_list:
+    for i, df in enumerate(df_list):
         df = df.copy()
         df["window"] = df[["seconds_elapsed", "data_set"]].apply(lambda row: math.floor((row["seconds_elapsed"] - seconds_elapsed_min[row["data_set"]]) / windows_size[row["data_set"]]), axis=1)
         df = df.sort_values(["data_set", "window", "seconds_elapsed"])
         df.drop("seconds_elapsed", axis=1, inplace=True)
-        df = df.groupby(["data_set", "window"], group_keys=True).apply(lambda x: np.array(x[[column for column in df.columns if column not in {"data_set", "window"}]])).to_frame()
+        df = (
+            df
+            .groupby(["data_set", "window"], group_keys=True)
+            .apply(lambda x: np.array(x[[column for column in df.columns if column not in {"data_set", "window"}]]))
+        ).to_frame()
+        df[0] = df.apply(lambda x: pad(x, count_after_padding))
+        df.rename(columns={0: i}, inplace=True)
         df_list_temp.append(df)
     # join the dataframe from all the dataset together according to index "windows" and "data_set"
     df_concat_temp = pd.concat(df_list_temp, axis=1)
@@ -116,23 +135,23 @@ for windows_count in windows_count_list:
 df_concat = pd.concat(df_concat_list)
 df_concat.reset_index(inplace=True)
 # Make the data_set the last column
-temp = df_concat.pop("data_set")
-df_concat["data_set"] = temp
 data_set_mappping = {
-    "Jumping_Jack": "Jumping_Jack",
-    "Lunges": "Lunges",
-    "Squat": "Squat",
-    "Jumping_Jack_new": "Jumping_Jack",
-    "Lunges_new": "Lunges",
-    "Squat_new": "Squat"
+    "Jumping_Jack": 0,
+    "Lunges": 1,
+    "Squat": 2,
+    "Jumping_Jack_new": 0,
+    "Lunges_new": 1,
+    "Squat_new": 2,
 }
 df_concat["exercise_type"] = df_concat["data_set"].map(data_set_mappping)
 df_concat.drop(["data_set"], axis=1, inplace=True)
 
 # %%
-df_concat["features"] = df_concat.apply(lambda x:np.concatenate([x[0], x[1], x[2]]), axis=1)
+df_concat["features"] = df_concat.apply(lambda x:np.concatenate([x[0], x[1], x[2]], axis=1), axis=1)
 df_concat.drop([0, 1, 2], axis=1, inplace=True)
-print(df_concat[0])
+temp = df_concat.pop("exercise_type")
+df_concat["exercise_type"] = temp
+print(df_concat)
 # %%
 # Split the data into train data and test data, to avoid train data mixed into test data spilit it by time series
 train_df = df_concat[(df_concat["window"] + 1) / df_concat["windows_count"] < 0.8].copy()
@@ -146,15 +165,17 @@ print(test_df)
 def divide_Xy(df, oversample=True):
     data = df.to_numpy()
     X = data[:, :-1]
-    y = data[:, -1]
+    y = data[:, 1].astype(np.int32)
     if oversample:
         ros = RandomOverSampler(random_state=0)
         X, y = ros.fit_resample(X, y)
-    return data, X, y
+    X = np.stack([item[0] for item in X])
+    X = X.astype(np.float32)
+    return X, y
 
 # Divide the train data and test data into X and y
-train_data, train_X, train_y = divide_Xy(train_df)
-test_data, test_X, test_y = divide_Xy(test_df, oversample=False)
+train_X, train_y = divide_Xy(train_df)
+test_X, test_y = divide_Xy(test_df, oversample=False)
 
 # Print the data
 print("train_X:")
@@ -169,7 +190,127 @@ print(test_y)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device:{device}")
 class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv1d(13, 32, 3, padding=1)
+        self.conv2 = nn.Conv1d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool1d(2, 2)
+        self.fc1 = nn.Linear(64 * 50, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3)
+        self.softmax = nn.Softmax(dim=1)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.softmax(self.fc3(x))
+        return x
+    def predict(self, X):
+        X = torch.tensor(X, dtype=torch.float32, device=device)
+        with torch.no_grad():
+            outputs = self(X)
+            _, predicted = torch.max(outputs.data, 1)
+        return predicted.cpu().numpy()
 
+# Train the model with train_X and train_y
+def train_model(model, train_X, train_y, epochs=10, batch_size=32, lr=0.001):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    model.to(device)
+    train_X = torch.tensor(train_X, dtype=torch.float32, device=device)
+    train_y = torch.tensor(train_y, dtype=torch.long, device=device)
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i in range(0, len(train_X), batch_size):
+            inputs = train_X[i:i+batch_size]
+            labels = train_y[i:i+batch_size]
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f"Epoch {epoch + 1} loss: {running_loss / len(train_X)}")
+    print("Finished Training")
+    return model
 
-evaluate_model(svm_model, test_X, test_y)
+# Call train_model to train the model
+cnn_model = CNN()
+train_X_CNN = train_X.transpose(0, 2, 1)
+train_y_CNN = train_y
+test_X_CNN = test_X.transpose(0, 2, 1)
+test_y_CNN = test_y
+cnn_model = train_model(cnn_model, train_X_CNN, train_y_CNN)
+def evaluate_model(model, X, y):
+    predicted_y = model.predict(X)
+    print(classification_report(y, predicted_y))
+
+    confusion_mat = confusion_matrix(y, predicted_y)
+    print(f"Confusion matrix: \n{confusion_mat}")
+evaluate_model(cnn_model, test_X_CNN, test_y_CNN)
+# %%
+class GRU(nn.Module):
+    def __init__(self):
+        super(GRU, self).__init__()
+        self.gru = nn.GRU(13, 64, 1, batch_first=True)
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 3)
+    def forward(self, x):
+        x, _ = self.gru(x)
+        x = self.fc1(x[:, -1, :])
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+    def predict(self, X):
+        X = torch.tensor(X, dtype=torch.float32, device=device)
+        with torch.no_grad():
+            outputs = self(X)
+            _, predicted = torch.max(outputs.data, 1)
+        return predicted.cpu().numpy()
+gru_model = GRU()
+train_X_GRU = train_X
+train_y_GRU = train_y
+test_X_GRU = test_X
+test_y_GRU = test_y
+gru_model = train_model(gru_model, train_X_GRU, train_y_GRU, epochs=35)
+evaluate_model(gru_model, test_X_GRU, test_y_GRU)
+# %%
+class CNN_GRU(nn.Module):
+    def __init__(self):
+        super(CNN_GRU, self).__init__()
+        self.conv1 = nn.Conv1d(13, 32, 3, padding=1)
+        self.conv2 = nn.Conv1d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool1d(2, 2)
+        self.gru = nn.GRU(64, 64, 2, batch_first=True)
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 3)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.transpose(2, 1)
+        x, _ = self.gru(x)
+        x = self.fc1(x[:, -1, :])
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+    def predict(self, X):
+        X = torch.tensor(X, dtype=torch.float32, device=device)
+        with torch.no_grad():
+            outputs = self(X)
+            _, predicted = torch.max(outputs.data, 1)
+        return predicted.cpu().numpy()
+
+cnn_gru_model = CNN_GRU()
+train_X_CNN_GRU = train_X_CNN
+train_y_CNN_GRU = train_y_CNN
+test_X_CNN_GRU = test_X_CNN
+test_y_CNN_GRU = test_y_CNN
+cnn_gru_model = train_model(cnn_gru_model, train_X_CNN_GRU, train_y_CNN_GRU, epochs=30)
+evaluate_model(cnn_gru_model, test_X_CNN_GRU, test_y_CNN_GRU)
 # %%
